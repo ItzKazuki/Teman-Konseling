00567ccf94c06954b96b512b6f09dd3517dfac85
Chaeza Ibnu Akbar <69-chaezaibnuakbar@users.noreply.gitlab.dev-app.online>
2025-12-07 00:29:53 +0700

    fix: fix layout dashboard

--- frontend/app/layouts/default.vue
+++ frontend/app/layouts/default.vue
@@ -10,15 +10,12 @@
         <NuxtLink v-for="item in navItems" :key="item.name" :to="item.to" :class="getLinkClass(item.to)">
           <Icon :name="item.icon" class="w-5 h-5 mr-3" />
           {{ item.name }}
-
-          <Icon v-if="item.dropdown" name="tabler:chevron-down" class="w-4 h-4 ml-auto" />
-          <Icon v-else-if="item.chevron" :name="item.chevron" class="w-4 h-4 ml-auto" />
         </NuxtLink>
 
         <div class="pt-4 mt-4 border-t border-gray-100 space-y-2">
-          <p class="text-xs font-semibold text-gray-400 uppercase tracking-wider px-3">INSIGHTS</p>
+          <p class="text-xs font-semibold text-gray-400 uppercase tracking-wider px-3">Master Data</p>
 
-          <NuxtLink v-for="item in insightItems" :key="item.name" :to="item.to" :class="getLinkClass(item.to)">
+          <NuxtLink v-for="item in masterData" :key="item.name" :to="item.to" :class="getLinkClass(item.to)">
             <Icon :name="item.icon" class="w-5 h-5 mr-3" />
             {{ item.name }}
           </NuxtLink>
@@ -28,7 +25,7 @@
 
     <main class="flex-1 overflow-y-auto p-8">
       <header class="mb-8 flex justify-between items-center">
-        <h2 class="text-2xl font-semibold">Acquisition Summary Overview</h2>
+        <h2 class="text-2xl font-semibold">Dashboard</h2>
         <div class="flex items-center space-x-4">
           <div class="flex items-center space-x-2">
             <img src="http://api.sistem-kinerja.test/static/profile.png" alt="User Avatar"
@@ -56,63 +53,30 @@ const navItems = [
     icon: 'tabler:dashboard'
   },
   {
-    name: 'Application',
-    to: '/application',
+    name: 'Moods',
+    to: '/moods',
     icon: 'tabler:layout-dashboard',
-    dropdown: true
-  },
-  {
-    name: 'Invoices',
-    to: '/invoices',
-    icon: 'tabler:file-invoice'
-  },
-  {
-    name: 'Reports',
-    to: '/reports',
-    icon: 'tabler:report-analytics'
-  },
-  {
-    name: 'Configuration',
-    to: '/configuration',
-    icon: 'tabler:settings'
   },
 
   // Kategori dengan sub-menu/chevron kanan
   {
-    name: 'Contact',
-    to: '/contact',
-    icon: 'tabler:users',
-    chevron: 'tabler:chevron-right'
-  },
-  {
-    name: 'Expense',
-    to: '/expense',
-    icon: 'tabler:wallet',
-    chevron: 'tabler:chevron-right'
-  },
-  {
-    name: 'Income',
-    to: '/income',
-    icon: 'tabler:trending-up',
-    chevron: 'tabler:chevron-right'
-  },
-  {
     name: 'User',
-    to: '/user',
+    to: '/users',
     icon: 'tabler:user-circle',
-    chevron: 'tabler:chevron-right'
   },
   {
-    name: 'Settings',
-    to: '/settings',
+    name: 'Siswa',
+    to: '/students',
     icon: 'tabler:adjustments',
-    chevron: 'tabler:chevron-right'
   },
 ]
 
-const insightItems = [
-  { name: 'Notification', to: '/notifications', icon: 'tabler:bell' },
-  { name: 'Inbox', to: '/inbox', icon: 'tabler:inbox' },
+const masterData = [
+  {
+    name: 'Kelas',
+    to: '/master-data/classrooms',
+    icon: 'tabler:bell'
+  },
 ]
 
 const getLinkClass = (itemPath: string) => {
