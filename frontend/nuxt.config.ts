135759369c2cce88c2ff318205d2225b6b54f51b
Chaeza Ibnu Akbar <69-chaezaibnuakbar@users.noreply.gitlab.dev-app.online>
2025-12-29 21:34:06 +0700

    fix: remove devtools in dashboard admin

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   css: ["~/assets/css/main.css"],
 
   ssr: false,
